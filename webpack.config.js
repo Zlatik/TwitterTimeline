6c59ae0bc8f99569c3bd3222ab7013f63efe2499
Zlatik <svyat161296@gmail.com>
2018-04-02 14:00:39 +0300

    config v2.0

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,7 @@ module.exports = {
         port:8080,
         inline:false,
         proxy:{
-            "api":"http://localhost:4006"
+            "proxy":`0.0.0.0:${process.env.port}`
         }
     },
     resolve:{
